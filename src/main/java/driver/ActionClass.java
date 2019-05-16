57e687101371a19dbf82dee89007683f38af2d36
deepaveetil <deeparaveendran@adqura.com>
2019-05-16 16:13:54 +0530

    updated the csvDownloadClick

--- src/main/java/driver/ActionClass.java
+++ src/main/java/driver/ActionClass.java
@@ -244,9 +244,42 @@ public class ActionClass extends TestBaseClass {
 
 		} catch (TimeoutException e) {
 
-			System.out.println("Are you sure the element is present");
-			flag = false;
-			return flag;
+			try {
+				boolean ele = new WebDriverWait(driver, 400).until(ExpectedConditions.and(
+						ExpectedConditions.visibilityOfElementLocated(By.xpath(prop.getProperty(object))),
+						ExpectedConditions.elementToBeClickable(By.xpath(prop.getProperty(object)))));
+				if (ele == true) {
+
+					WebElement element = driver.findElement(By.xpath(prop.getProperty(object)));
+
+					// Actions action = new Actions(driver);
+					System.out.println("is this the element???>>" + element);
+					element.click();
+					// action.moveToElement(element).click(element).build().perform();
+
+					Thread.sleep(5000);
+
+					// action.moveToElement(element).sendKeys(Keys.RETURN);
+					// driver.findElement(By.xpath(prop.getProperty(object))).click();
+					if (driver.getPageSource().equalsIgnoreCase("Data Set BatchDecisionOutput is empty")) {
+						flag = false;
+						System.out.println("Dataset is empty");
+						return flag;
+					}
+					flag = true;
+				} else {
+					System.out.println("run engine is not clicked..");
+					flag = false;
+					return flag;
+				}
+			} catch (Exception e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+				System.out.println("Are you sure the element is present");
+				flag = false;
+				return flag;
+			}
+
 			// TODO: handle exception
 		} catch (Exception e) {
 			System.out.println("element is not present" + " " + e.getMessage());
